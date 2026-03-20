cc53458574ca24d3c4ef286e9bd2e9815200a822
Justine-Web-Dev <ramirezjustine815@gmail.com>
2026-03-20 17:55:48 +0800

    Add files via upload

---
+++ backend/project/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import register_user,login_user, get_user
+
+urlpatterns = [
+    path('register_user/', register_user,name='register_user'),
+    path("login_user/", login_user,name='login_user'),
+    path("get_user/", get_user,name='get_user')
+]
---
+++ backend/project/users/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from .serializers import UsersSerializers
+from .models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+import jwt
+import datetime
+from django.conf import settings
+
+# Create your views here.
+
+@api_view(['GET'])
+def get_user(request):
+  users = User.objects.all()
+  serializers = UsersSerializers(users,many=True)
+  return Response(serializers.data)
+
+@api_view(['POST'])
+def register_user(request):
+  serializers = UsersSerializers(data=request.data)
+
+  if serializers.is_valid():
+    serializers.save()
+    return Response(serializers.data, status=status.HTTP_201_CREATED)
+  return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login_user(request):
+  # extract credentials from payload
+  username = request.data.get('username')
+  password = request.data.get('password')
+
+  # look up user by username
+  try:
+    user = User.objects.get(username=username)
+  except User.DoesNotExist:
+    return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+  
+  # simple plaintext check (should be hashed in production)
+  if user.password != password:
+    return Response({'error': 'Invalid password'}, status=status.HTTP_400_BAD_REQUEST)
+  
+  payload = {
+    "user_id": user.id,
+    "username": user.username,
+    "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+  }
+  
+  token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+
+  return Response({
+    "token": token,
+    "username": user.username,
+    # you could add additional fields here (is_admin, etc.)
+  })
