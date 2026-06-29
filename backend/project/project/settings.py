e69729158609a040a99cadaed24a10d2f71ed6f3
PujiWhap <ramirezjustine815@gmail.com>
2026-06-29 14:46:14 +0800

    Updated port

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'USER': os.environ.get("USER"),
         'PASSWORD': os.environ.get("PASSWORD"),
         'HOST': os.environ.get("HOST"),
-        'PORT': os.environ.get("PORT"),
+        'PORT': os.environ.get("DB_PORT","5432"),
         'OPTIONS': {
             'connect_timeout': 10,  
         },
