8eeb7e0d82a00ac5de230cef11aabb17251fed7e
PujiWhap <ramirezjustine815@gmail.com>
2026-08-07 16:23:15 +0800

    Updated

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,6 +1,5 @@
 import axios from "axios"; 
-//||"http://127.0.0.1:8000/api"
-const baseURL = (import.meta.env.VITE_API_URL).replace(/\/$/, "");
+const baseURL = (import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api").replace(/\/$/, "");
 
 export const api = axios.create({
   baseURL,
